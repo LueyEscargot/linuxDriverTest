052a1ef815a31e954e622a761df68423f52d065d
Liu Yu <source@liuyu.com>
2020-03-10 22:23:01 +0800

    update driver.h

--- lesson_02_characterDevice/driver.h
+++ lesson_02_characterDevice/driver.h
@@ -1,19 +1,3 @@
-/*
- * scull.h -- definitions for the char module
- *
- * Copyright (C) 2001 Alessandro Rubini and Jonathan Corbet
- * Copyright (C) 2001 O'Reilly & Associates
- *
- * The source code in this file can be freely used, adapted,
- * and redistributed in source or binary form, so long as an
- * acknowledgment appears in derived source files.  The citation
- * should list that the code comes from the book "Linux Device
- * Drivers" by Alessandro Rubini and Jonathan Corbet, published
- * by O'Reilly & Associates.   No warranty is attached;
- * we cannot take responsibility for errors or fitness for use.
- *
- * $Id: scull.h,v 1.11 2001/07/18 22:28:18 rubini Exp $
- */
 
 #ifndef __DRIVER_H__
 #define __DRIVER_H__
@@ -32,6 +16,9 @@ typedef struct Test_Dev
      struct semaphore sem;
 } Test_Dev;
 
-#define SCULL_MAX_TYPE 5
+#define TEST_MAX_TYPE 1
+#ifndef TEST_NR_DEVS
+#define TEST_NR_DEVS 4
+#endif
 
 #endif /* __DRIVER_H__ */
